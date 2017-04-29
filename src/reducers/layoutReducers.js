c8d25bc6d6b0d3e86f50f0910fca002556838514
Mina S. Aziz <minaz922@gmail.com>
2017-04-29 22:21:38 +0200

    call parseMarkdown() from reducer when 'PARSE_MARKDOWN' action is called and update state accordingly

--- src/reducers/layoutReducers.js
+++ src/reducers/layoutReducers.js
@@ -1,9 +1,13 @@
-export default (state= [], action) => {
+import parseMarkdown from '../utils/parseMarkdown';
+
+let initialState = {
+  parseMarkdown: ''
+};
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case 'PARSE_MARKDOWN':
-
-
-      return Object.assign({}, action.parsedMarkdown);
+      return {parsedMarkdown: parseMarkdown(action.payload.input)};
     default:
       return state;
   }
